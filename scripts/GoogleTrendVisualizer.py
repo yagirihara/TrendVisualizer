cb9d029ed1a7ce25b903928c533861e1de486ecd
Unknown <yagiri.hideki@gmail.com>
2019-01-05 22:19:10 +0900

    Modify some points.
    Put on data csv file.
    Reading CSV file, inserting various info.
    Change standard keyword.

--- scripts/GoogleTrendVisualizer.py
+++ scripts/GoogleTrendVisualizer.py
@@ -1,7 +1,9 @@
 # coding: utf-8
+import pandas as pd
 from pytrends.request import TrendReq
 from itertools import zip_longest, filterfalse
 from InsertElasticsearch import bulk_insert
+
 # import matplotlib as mpl
 # import pandas as pd
 # import japanize_matplotlib
@@ -9,17 +11,19 @@ from InsertElasticsearch import bulk_insert
 INDEX_NAME = "smash_bros_trend"
 pytrends = TrendReq(hl='ja-JP', tz=360)
 
-# 100になったものをベースにしないと値が変わる→共通で入れておく。
-CHARACTER_LIST = ["マリオ", "ドンキーコング", "リンク", "サムス", "ダークサムス", "ヨッシー", "カービィ", "フォックス", "ピカチュウ", "ルイージ", "ネス", "キャプテンファルコン", "プリン", "ピーチ", "デイジー", "クッパ", "アイスクライマー", "シーク", "ゼルダ", "ドクターマリオ", "ピチュー", "ファルコ", "マルス", "ルキナ", "こどもリンク", "ガノンドロフ", "ミュウツー", "ロイ", "クロム", "Mr.ゲーム＆ウォッチ", "メタナイト", "ピット", "ブラックピット", "ゼロスーツサムス", "ワリオ", "スネーク", "アイク", "ポケモントレーナー", "ディディーコング", "リュカ", "ソニック", "デデデ", "ピクミン＆オリマー", "ルカリオ", "ロボット", "トゥーンリンク", "ウルフ", "むらびと", "ロックマン", "WiiFitトレーナー", "ロゼッタ＆チコ", "リトルマック", "ゲッコウガ", "Miiファイター", "パルテナ", "パックマン", "ルフレ", "シュルク", "クッパJr.", "ダックハント", "リュウ", "ケン", "クラウド", "カムイ", "ベヨネッタ", "インクリング", "リドリー", "シモン", "リヒター", "キングクルール", "しずえ", "ガオガエン"]
+df_character = pd.read_csv('data/characterData.csv', index_col=0)
+
+# print(df_character.loc['リンク']['series'])
+
+CHARACTER_LIST = df_character.index
 PREFIX_WORD = "スマブラ SP "
 timeframe_val = '2018-12-07 2018-12-31'
 
 search_character_list = []
 for character in CHARACTER_LIST:
     search_character_list.append(PREFIX_WORD+character)
-print(search_character_list)
 
-base_character = "ネス"
+base_character = "ガノンドロフ"
 
 group_by = 4
 chunks = zip_longest(*[iter(search_character_list)]*group_by)
@@ -48,10 +52,13 @@ for elems in list(chunks):
 json_list = []
 for date_val, series in df_result.iterrows():
     for character, value in series.iteritems():
-        insert_json = {'@timestamp': int(date_val.timestamp(
-        )), 'character_name': '', 'series': '', 'smash_bros_ver': '', 'value': 0}
-        insert_json['character_name'] = character.replace(PREFIX_WORD, "")
-        insert_json['value'] = value
+        character_name = character.replace(PREFIX_WORD, "")
+        insert_json = {
+                '@timestamp': int(date_val.timestamp()),
+                'character_name': character_name,
+                'series': df_character.loc[character_name]['series'],
+                'smash_bros_ver': df_character.loc[character_name]['first'],
+                'value': value}
         json_list.append(insert_json)
 
 bulk_insert(INDEX_NAME, json_list)
